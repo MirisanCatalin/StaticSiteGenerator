dff11b198a920f4c39f969e58f28ecc78860c901
Mirisan Catalin <catalinmirisan1@gmail.com>
2025-10-23 09:37:44 +0300

    adding enums

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode, Bender
+
+def main():
+    ceva = TextNode("This is some anchor text", "link", "https://www.boot.dev")
+    print(ceva)
+
+if __name__ == "__main__":
+    main()
