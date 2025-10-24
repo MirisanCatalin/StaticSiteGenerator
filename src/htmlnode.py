ce68e75246ebdddca84752dc0cfbdc657f5ed531
Mirisan Catalin <catalinmirisan1@gmail.com>
2025-10-24 20:34:39 +0300

    Prnet Node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -31,7 +31,25 @@ class LeafNode(HTMLNode):
             return self.value
         else:
             return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"LeafNode(tag={self.tag}, value={self.value}, props={self.props})"
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag,children, props=None):
+        super().__init__(tag, None, children, props)
 
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("ParentNode must have a tag to convert to HTML")
+        if not self.children:
+            raise ValueError("ParentNode must have children to convert to HTML")
+        children_html = "" 
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
 
+    def __repr__(self):
+        return f"ParentNode(tag={self.tag}, children={self.children}, props={self.props})"
 
 
